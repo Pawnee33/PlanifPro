ff31aca1c2ccf0301c6c743ef783cdd669b95373
Pawnee <pawneedefize@hotmail.com>
2026-06-16 17:06:07 +0200

    feat: routage (react-router) + connexion fonctionnelle (appel API)

--- planifPro/frontend/src/App.jsx
+++ planifPro/frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Connexion from './pages/Connexion'
 import Inscription from './pages/Inscription'
 
 function App() {
-  return <Inscription />
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/connexion" replace />} />
+      <Route path="/connexion" element={<Connexion />} />
+      <Route path="/inscription" element={<Inscription />} />
+    </Routes>
+  )
 }
 
 export default App
