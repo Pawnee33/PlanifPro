ca64570cfa43e335d804e3133217f1dca032f212
Pawnee <pawneedefize@hotmail.com>
2026-06-19 11:03:49 +0200

    chore: allonger la durée de vie du token JWT à 8h

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
 from sqlalchemy.pool import StaticPool
 
@@ -12,6 +13,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=8)
 
 
 class DevelopmentConfig(Config):
